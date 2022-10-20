6e96b9ac398ff1f80940bbcd4ce19acd523870ca
RahulFernando <rahulrulz680@gmail.com>
2022-10-20 10:38:14 +0530

    sign up firebase

--- lib/screens/signUp.dart
+++ lib/screens/signUp.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:research_blogger/constants.dart';
+import 'package:research_blogger/models/response.dart';
+import 'package:research_blogger/models/user.dart';
+import 'package:research_blogger/service/authService.dart';
+import 'package:research_blogger/service/userService.dart';
 
 import '../utils/colorUtils.dart';
 import '../widgets/AuthOption.dart';
@@ -21,6 +25,8 @@ class _SignUpState extends State<SignUp> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
   final TextEditingController _passwordNameController = TextEditingController();
+
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -47,32 +53,122 @@ class _SignUpState extends State<SignUp> {
                 Text(
                   "Sign Up",
                   style:
-                  GoogleFonts.ptSans(fontSize: 22.0, color: Colors.white),
+                      GoogleFonts.ptSans(fontSize: 22.0, color: Colors.white),
                 ),
                 const SizedBox(
                   height: 50,
                 ),
                 reusableTextField("Enter Username", Icons.verified_user, false,
                     _userNameController, TextInputType.name),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 reusableTextField("Enter Email", Icons.email_rounded, false,
                     _emailController, TextInputType.emailAddress),
-                const SizedBox(height: 20,),
-                reusableTextField("Enter Age", Icons.verified_user, false,
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Age", Icons.timelapse, false,
                     _ageController, TextInputType.text),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 reusableTextField("Enter Password", Icons.lock, true,
                     _passwordNameController, TextInputType.visiblePassword),
-                const SizedBox(height: 20,),
-                reusableAuthButton(context, "SIGN UP", () => {}),
-                const SizedBox(height: 25,),
-                authOption("Already have an account? ", "Sign In", () => Navigator.pushNamed(context, SIGN_IN_SCREEN)),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 20,
+                ),
+                loading
+                    ? const CircularProgressIndicator()
+                    : reusableAuthButton(context, "SIGN UP", () async {
+                        var validity = validate(
+                            _emailController.text,
+                            _userNameController.text,
+                            _ageController.text,
+                            _passwordNameController.text);
+
+                        if (!validity.isValid) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(validity.message),
+                            backgroundColor: Colors.redAccent,
+                          ));
+                        }
+
+                        if (validity.isValid) {
+                          setState(() {
+                            loading = true;
+                          });
+                          var response = await AuthService.register(
+                              email: _emailController.text,
+                              password: _passwordNameController.text);
+
+
+                          if (response.status == 201) {
+                            var res = await UserService.create(User(
+                                uid: response.data.uid,
+                                userName: _userNameController.text,
+                                age: _ageController.text));
+
+                            print(res.status);
+
+                            if (res.status == 201) {
+                              setState(() {
+                                loading = false;
+                              });
+
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(response.message as String),
+                                backgroundColor: Colors.green,
+                                duration: Duration(seconds: 3),
+                              ));
+
+                              Navigator.pushNamed(context, SIGN_IN_SCREEN);
+                            }
+
+                            if (res.status == 500) {
+                              setState(() {
+                                loading = false;
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(response.message as String),
+                                backgroundColor: Colors.redAccent,
+                              ));
+                            }
+                          }
+
+                          if (response.status == 500) {
+                            setState(() {
+                              loading = false;
+                            });
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(response.message as String),
+                              backgroundColor: Colors.redAccent,
+                            ));
+                          }
+                        }
+                      }),
+                const SizedBox(
+                  height: 25,
+                ),
+                authOption("Already have an account? ", "Sign In",
+                    () => Navigator.pushNamed(context, SIGN_IN_SCREEN)),
+                const SizedBox(
+                  height: 10,
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Validate validate(
+      String email, String userName, String age, String password) {
+    if (email.isEmpty || password.isEmpty || userName.isEmpty || age.isEmpty) {
+      return Validate(isValid: false, message: "All fields are required");
+    }
+
+    return Validate(isValid: true, message: "");
   }
 }
